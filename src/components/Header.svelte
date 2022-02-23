4da5bb96ad07d4ed6c6c4cd89a4fb491985fc663
Alan <john1337@gmail.com>
2022-02-23 17:16:59 -0600

    Sass en svelte

---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+	<nav>
+		<h1>
+			<a href="/" class="logo">HOME</a>
+		</h1>
+		<ul>
+			<li><a href="/">Fode</a></li>
+			<li><a href="/">Acerca de</a></li>
+			<li><a href="/">Menu</a></li>
+			<li><a href="/">Registro</a></li>
+			<li><a href="/" class="btn btn-cta">Login</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="scss">
+	a {
+		color: $secondary-color;
+		text-decoration: none;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Header from '../components/Header.svelte';
+</script>
+
+<body>
+	<div class="contenedor">
+		<Header />
+		<div class="main-screen">
+			<div class="row">
+				<div class="col-30 m-9">
+					<h3>Comida italiana</h3>
+					<h2 class="py-4">Con el mejor sabor.</h2>
+					<p class="">Manteniendo las mejores recetas del país de origen</p>
+
+					<div class="row py-4">
+						<div class="col">
+							<div class="prices w-75 m-auto">
+								<div class="prev">$7.90</div>
+								<div class="now">$6.90</div>
+							</div>
+						</div>
+						<div class="col">
+							<div>
+								<a href="/" class="btn btn-cta shadow"> ORDENAR </a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-70">
+					<img src="salad.jpg" width="90%" alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+<style lang="scss">
+	body {
+		background-color: $primary-color;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+</style>
